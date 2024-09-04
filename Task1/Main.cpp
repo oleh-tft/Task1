006b0b40fd368234ccf2aaea5a1cce73d0645535
oleh-tft <tiftelalbion@gmail.com>
2024-09-04 13:51:25 +0300

    Added Fraction class with Init and Print methods

--- Task1/Main.cpp
+++ Task1/Main.cpp
@@ -2,7 +2,28 @@
 
 using namespace std;
 
+class Fraction
+{
+private:
+	int numerator;
+	int denominator;
+
+public:
+	void Init() {
+		cout << "Input Numerator: ";
+		cin >> numerator;
+		cout << "Input Denominator: ";
+		cin >> denominator;
+	}
+
+	void Print() {
+		cout << "Fraction: " << numerator << "/" << denominator << endl;
+	}
+};
+
 int main()
 {
-
+	Fraction fraction;
+	fraction.Init();
+	fraction.Print();
 }
